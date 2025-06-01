b30b51e401abbee77333133a81698e0aa095841f
leonardo-ece <eceleo01@students.zhaw.ch>
2025-06-01 10:38:11 +0200

    update

--- src/routes/stats/+page.server.js
+++ src/routes/stats/+page.server.js
@@ -6,10 +6,8 @@ export async function load() {
   const trips = await db.collection('trips').find().toArray();
   const destinations = await db.collection('destinations').find().toArray();
 
-  // Mappings
   const destinationMap = Object.fromEntries(destinations.map(d => [d._id.toString(), d]));
 
-  // KPIs
   const totalTrips = trips.length;
   const uniqueDestinations = new Set(trips.map(t => t.destinationId)).size;
   const avgRating = trips.reduce((sum, t) => sum + (t.rating || 0), 0) / totalTrips || 0;
@@ -24,7 +22,6 @@ export async function load() {
 
   const mostVisitedCountry = Object.entries(countryCount).sort((a, b) => b[1] - a[1])[0]?.[0] ?? '-';
 
-  // Durchschnittliche Dauer
   const avgDays =
     trips
       .map(t => (new Date(t.endDate) - new Date(t.startDate)) / (1000 * 60 * 60 * 24))
